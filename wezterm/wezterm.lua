8f05bcd970e85ecc067463af85cbd452ea293fa3
Mathias Balling <math260r@gmail.com>
2024-02-17 11:55:27 +0100

    Add wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require("wezterm")
+
+local config = {
+	font_size = 20,
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+
+	colors = {
+		cursor_bg = "#57bfff",
+		cursor_fg = "#1e1e1e",
+		selection_bg = "#57bfff",
+		selection_fg = "#1e1e1e",
+	},
+
+	enable_tab_bar = false,
+	-- If tab bar is enabled:
+	hide_tab_bar_if_only_one_tab = true,
+	show_tab_index_in_tab_bar = false,
+	show_new_tab_button_in_tab_bar = false,
+	tab_bar_style = {},
+}
+return config
