ccfd2c5932cf2bf21642be72ed4e9bd287256431
Mathias Balling <math260r@gmail.com>
2025-03-27 20:44:05 +0100

    Update yazi keybinds

--- nvim/lua/plugins/yazi.lua
+++ nvim/lua/plugins/yazi.lua
@@ -23,12 +23,17 @@ return {
     -- if you want to open yazi instead of netrw, see below for more info
     open_for_directories = false,
     keymaps = {
-      change_working_directory = "<c-/>",
-      show_help = "<c-?>",
+      change_working_directory = "<c-w>",
+      show_help = "<c-h>",
     },
     integrations = {
       grep_in_directory = "snacks.picker",
       grep_in_selected_files = "snacks.picker",
     },
   },
+  -- init = function()
+  --   -- Block netrw plugin load
+  --   -- vim.g.loaded_netrw = 1
+  --   vim.g.loaded_netrwPlugin = 1
+  -- end,
 }
